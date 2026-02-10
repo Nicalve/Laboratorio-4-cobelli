d2adf73bc89a879af27b79d1d6c1c9a122d333ae
Nicolás Clotta <nicolas.clotta@gmail.com>
2026-02-10 09:43:31 -0300

    archivos .py clase 5

---
+++ Clase 5/analisis.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  9 18:33:36 2026
+
+@author: nclotta
+"""
+
+# Time-stamp: </Users/nclotta/Documents/__UBA/__LABO_4_CUATRO/informe2/analisis.py, 2026-02-09 Monday 18:43:39 nclotta>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# eof
---
+++ Clase 5/medicion.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 10 09:06:36 2026
+
+@author: nclotta
+"""
+
+# Time-stamp: <>
+
+import numpy as np
+import pandas as pd
+import time
+import matplotlib.pyplot as plt
+
+import SR830
+
+lockin = SR830("GPIB0::8::INSTR")
+
+voltaje = 0.5
+
+def medicion_frecuencias(lockin, minf=200, maxf=100000, num=20):
+    lockin.auto_scale()
+    lockin.get_medicion(isXY=True)
+    lockin.set_time_constant(10)
+    
+    frecuencias = np.geomspace(minf, maxf, num)
+    medicion = []
+    nespera = 5
+    tespera = lockin.time_constant_values[lockin.time_constant] * nespera
+    
+    for freq in frecuencias:
+        lockin.set_referencia(isIntern=True, freq=freq, voltaje=0.5)
+        lockin.auto_scale()
+    
+        medicion.append(lockin.get_medicion(isXY=True))
+        time.sleep(tespera)
+    
+        plt.plot(np.log(frecuencias), [np.abs(medicion[i][0]) for i in range(len(medicion))], 'o-')
+    return frecuencias, [np.abs(medicion[i][0]) for i in range(len(medicion))]
+
+    
+
+def medicion_polares(lockin, N=10, freq=1500):
+    lockin._lockin.write("OFLT 10")
+    lockin._lockin.write("OFSL 2")
+    lockin._lockin.write("SLVL {0:f}".format(voltaje))
+    
+    waitt = lockin.time_constant_values[lockin.get_time_constant()] * 5
+    
+    r0 = np.zeros(N)
+    tita = np.zeros(N)
+
+    lockin._lockin.write("FMOD 1")
+    lockin._lockin.write("FREQ {0:f}".format(freq))
+    lockin.auto_scale()
+    
+    for i in range(N):
+        time.sleep(waitt)
+        r0[i], tita[i] = lockin.auto_scale()
+    return r0, tita
+
+def res_cable(r0):
+    V_0 = voltaje
+    R_res = 1000  # Ohm
+    V = np.mean(np.abs(r0))
+    return R_res / ((V_0/V)-1)
+    
+def resistividad(r0):
+    return res_cable(r0) * ((0.0004)**2 * np.pi) / (0.85*2)
+
+def medicion_xy(lockin, N=10, freq=1500):
+    lockin._lockin.write("OFLT 9")
+    lockin._lockin.write("OFSL 2")
+    lockin._lockin.write("SLVL {0:f}".format(voltaje))
+    
+    waitt = lockin.time_constant_values[lockin.get_time_constant()] * 5
+    
+    r0 = np.array([])
+    
+    lockin._lockin.write("FMOD 1")
+    lockin._lockin.write("FREQ {0:f}".format(freq))
+    lockin.auto_scale()
+    
+    for i in range(N):
+        time.sleep(waitt)
+        lockin.auto_scale()
+        r0 = np.append(r0, lockin.get_medicion())
+    return r0
+
+# eof
