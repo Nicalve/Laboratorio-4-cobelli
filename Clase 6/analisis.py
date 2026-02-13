489b1067e4bf142e32c5b4c6d6874fc40533c70e
Gustavo <lucasvelas15@gmail.com>
2026-02-13 19:47:40 -0300

    Mis cambios en analisis.py

--- Clase 6/analisis.py
+++ Clase 6/analisis.py
@@ -11,7 +11,12 @@ Created on Mon Feb  9 18:33:36 2026
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from itertools import product
 
+
+ROOT = Path(__file__).resolve().parents[1] #asi funciona para spyder
+
+#
 # for i, x in enumerate(df[0]):
 #     print(f"Index {i}: {x}")
 voltaje = 0.5
@@ -29,22 +34,106 @@ def resistividad(r0, L):
 #[0, "time cte"],
 #datos_segun_L = [[0.26, "L26"], [0, "run_xy"], [0.26, "L26_1"]]
 #datos_segun_L = [[0, "xy_tc7"]]
-datos_segun_L = [[0.969, "L_96_9"]]
+#datos_segun_L = [[0.969, "L_96_9"]]
+datos_segun_L = [                 #no agregue L26 porque creo que era malo ese
+    [0.121, "L_12_1"],
+    [0.187, "L_18_7"],
+    [0.261, "L26_1"],
+    [0.270, "L_27_0"],
+    [0.360, "L_36"], 
+    [0.969, "L_96_9"], 
+    [1.060, "L_106"], 
+    [1.350, "L_135"]]
+
+todos_los_rhos = []
+
 for i, dtl in enumerate(datos_segun_L):
-    directory_path = Path("./datos_exp2/" + dtl[1])
+    directory_path = ROOT / "datos_exp2" / dtl[1] #asi funciona para spyder
     files = [item for item in directory_path.iterdir() if item.is_file()]
-    res = []    
+    res = []
+    rhos = []
+
+
+    print(f"\n=== Analizando {dtl} ===")
+    
     for file_path in files:
         df = pd.read_csv(file_path)
         df=df.T
         print(f"<X> = {np.mean(df[0])}, sigma_X = {np.std(df[0])}")
+        print(f"<Y> = {np.mean(df[1])}, sigma_Y = {np.std(df[1])}")
         print(f"R={res_cable(df[0])}, rho = {resistividad(df[0], dtl[0])}")
-    #    res.append(res_cable(df[0]))
+        res.append(res_cable(df[0]))
+        rhos.append(resistividad(df[0], dtl[0]))
+        
+        #print("res:", res)
+        #print("rhos:", rhos)    
     #print(f"Promedio R para {dtl[1]}: {np.mean(res)}, c = {resistividad(np.mean(res), 0)}")
 
+    todos_los_rhos.append(rhos) #es una lista de listas por cada L donde tiene todos los valores de rhos de cada L
+    #print(todos_los_rhos) 
+
+RHO_TAB = 1.68e-8
+
+print("\n=== Más cercano al tabulado ===")
+
+for i, rhos in enumerate(todos_los_rhos):
+
+    mejor = min(rhos, key=lambda x: abs(x - RHO_TAB))
+
+    L = datos_segun_L[i][0]
+
+    print(f"L = {L:} -> rho = {mejor:}")
+
+
+# juntamos todos
+todos = []
+
+for rhos in todos_los_rhos:
+    todos.extend(rhos)
+
+rho_prom = np.mean(todos)
+
+print("\nRho promedio:", rho_prom)
+
+
+print("\n=== Más parecidos entre sí ===")
+
+for i, rhos in enumerate(todos_los_rhos):
+
+    mejor = min(rhos, key=lambda x: abs(x - rho_prom))
+
+    L = datos_segun_L[i][0]
+
+    print(f"L = {L:.3f} -> rho = {mejor:.3e}")
 
 
 
+# Todas las combinaciones posibles
+combinaciones = list(product(*todos_los_rhos))
+
+print("Total combinaciones:", len(combinaciones))
+
+def dispersion(lista):
+    return max(lista) - min(lista)
+
+mejor = None
+mejor_disp = np.inf
+
+for combo in combinaciones:
+
+    d = dispersion(combo)
+
+    if d < mejor_disp:
+        mejor_disp = d
+        mejor = combo
+
+print("\n=== Mejor conjunto consistente ===")
+
+for i, rho in enumerate(mejor):
+    L = datos_segun_L[i][0]
+    print(f"L = {L} -> rho = {rho}")
+
+print("\nDispersión mínima:", mejor_disp)
 
 
 
